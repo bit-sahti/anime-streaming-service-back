5375aa48be90cabf903e201c88aed5f300c65682
Thaís Bitencourt <thais.bitencourt@yahoo.com>
2021-07-05 18:25:16 -0300

    add basic express set up

---
+++ index.js
@@ -0,0 +1,14 @@
+require('dotenv').config();
+require('./configs/mongodb.config');
+
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors( { origin: process.env.FRONT_URL } ));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.listen(process.env.PORT, () => console.log(`App listening on PORT ${process.env.PORT}`))
