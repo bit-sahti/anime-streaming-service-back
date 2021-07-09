c64542c78b3e7b47b480afb91fb22abc51aa3c17
Thaís Bitencourt <thais.bitencourt@yahoo.com>
2021-07-09 15:26:26 -0300

    refactor tests

--- tests/animeRoutes.test.js
+++ tests/animeRoutes.test.js
@@ -2,64 +2,56 @@ const mongoose = require("mongoose");
 const request = require("supertest");
 const app = require("../index");
 
-const Formatter = require("../utils/Formatter.utils");
+const formatter = require("../utils/formatter.utils");
 
 afterAll(() => mongoose.connection.close());
 
-let responseBody, responseStatus;
+let response;
 
 describe("Get /animes", () => {
   beforeAll(async () => {
-    const response = await request(app).get("/api/animes");
-
-    responseBody = response.body;
-    responseStatus = response.status;
+    response = await request(app).get("/api/animes")
   });
 
   it("should be a successfull request", () => {
-    expect(responseStatus).toEqual(200);
+    expect(response.status).toEqual(200);
   });
 
   it("should return an object with a data property containing an array", () => {
-    expect(responseBody).toEqual(
+    expect(response.body).toEqual(
       expect.objectContaining({ data: expect.any(Array) })
     );
   });
 
   it("should return all documents on DB", () => {
-    expect(responseBody.data.length).toEqual(6);
+    expect(response.body.data.length).toEqual(6);
   });
 });
 
 describe("Get animes/categories/genres", () => {
   let genre = "mahou shoujo";
 
-  beforeAll(async () => {
-    const response = await request(app).get(
+  beforeAll(async () => response = await request(app).get(
       "/api/animes/categories/" + encodeURIComponent(genre)
-    );
-
-    responseBody = response.body;
-    responseStatus = response.status;
-  });
+    ))
 
   it("should be a successfull request", () => {
-    expect(responseStatus).toEqual(200);
+    expect(response.status).toEqual(200);
   });
 
   it("should return an object with a data property containing an array", () => {
-    expect(responseBody).toEqual(
+    expect(response.body).toEqual(
       expect.objectContaining({ data: expect.any(Array) })
     );
   });
 
   it(`should return all ${genre} animes from the DB`, () => {
-    expect(responseBody.data.length).toEqual(2);
+    expect(response.body.data.length).toEqual(2);
   });
 
   it(`should not return any anime that is not a ${genre}`, () => {
-    const formattedGenre = Formatter.capitalize(genre);
-    const matchGenre = responseBody.data.every((anime) =>
+    const formattedGenre = formatter.capitalize(genre);
+    const matchGenre = response.body.data.every((anime) =>
       anime.genres.includes(formattedGenre)
     );
 
@@ -68,34 +60,27 @@ describe("Get animes/categories/genres", () => {
 });
 
 describe("Get call on animes/:id", () => {
-  let sampleAnime, unexistingIdReqBody, badRequestBody, badRequestStatus;
+  let sampleAnime, unexistingID, badRequest;
 
   beforeAll(async () => {
     const { body } = await request(app).get("/api/animes");
 
     sampleAnime = body.data[0];
 
-    const response = await request(app).get("/api/animes/" + sampleAnime._id);
-    const unexistingIDRes = await request(app).get(
+    response = await request(app).get("/api/animes/" + sampleAnime._id);
+
+    unexistingID = await request(app).get(
       "/api/animes/" + "6082d65fe511f735f5d25ec2"
     );
-    const badRequestRes = await request(app).get("/api/animes/" + "sdfghf");
-
-    responseBody = response.body;
-    responseStatus = response.status;
-
-    unexistingIdReqBody = unexistingIDRes.body;
-
-    badRequestBody = badRequestRes.body;
-    badRequestStatus = badRequestRes.status;
+    badRequest = await request(app).get("/api/animes/" + "sdfghf");
   });
 
   it("should be a successfull request", () => {
-    expect(responseStatus).toEqual(200);
+    expect(response.status).toEqual(200);
   });
 
   it("should have data containing anime and media properties", () => {
-    expect(responseBody).toEqual(
+    expect(response.body).toEqual(
       expect.objectContaining({
         data: {
           anime: expect.any(Object),
@@ -110,15 +95,15 @@ describe("Get call on animes/:id", () => {
   });
 
   it("should only return one anime", () => {
-    expect(Array.isArray(responseBody.data.anime)).toBeFalsy();
+    expect(response.body.data.anima && Array.isArray(response.body.data.anime)).toBeFalsy();
   });
 
   it("should return the anime with the requested id", () => {
-    expect(responseBody.data.anime._id).toEqual(sampleAnime._id);
+    expect(response.body.data.anime._id).toEqual(sampleAnime._id);
   });
 
   it("should return null properties if requested id doesn't exist", () => {
-    expect(unexistingIdReqBody).toEqual(
+    expect(unexistingID.body).toEqual(
       expect.objectContaining({
         data: {
           anime: null,
@@ -129,11 +114,11 @@ describe("Get call on animes/:id", () => {
   });
 
   it("should return a'bad request' status if :id isn't a string", () => {
-    expect(badRequestStatus).toEqual(400);
+    expect(badRequest.status).toEqual(400);
   });
 
   it("should return an error message in it's body if requested id isn't a string", () => {
-    expect(badRequestBody).toEqual(
+    expect(badRequest.body).toEqual(
       expect.objectContaining({
         error: {
           type: expect.any(String),
