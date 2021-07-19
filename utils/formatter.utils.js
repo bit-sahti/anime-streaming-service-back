db273e876f1c3165dd4d673c216f52c8eb623417
Thaís Bitencourt <thais.bitencourt@yahoo.com>
2021-07-19 18:19:13 -0300

    add notes

--- utils/formatter.utils.js
+++ utils/formatter.utils.js
@@ -1,7 +1,8 @@
 class Formatter {
     capitalize = string => {
         if (typeof string !== 'string') return '';
-        
+
+        //it won't work on words that actually have an hypen, like sci-fi        
         return string.split(/[-\s]/).map(word => word[0].toUpperCase() + word.slice(1)).join(' ')
     }
 
